79f395132710ca6d5028b84b7d2aa82d234990ef
Fomegne <fomegnemeudje@outlook.com>
2022-12-12 13:27:26 +0100

    fix: problem of serving of media file in production - Removed the serving of media file by django in production mode

--- django_form/urls.py
+++ django_form/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("", include("core.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns = [
         path("__debug__/", include('debug_toolbar.urls')),
-    ] + urlpatterns
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
